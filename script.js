93b18690d6d92b6699ba7de93613f8efc2bd6993
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-07-14 23:39:23 -0700

    checking logic of choosing an operand function, if the entered operand is valid a message confirming so will currently appear

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var readlineSync = require('readline-sync');
  
 // Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+
+const whichOp = () => {
+    const DIVIDE = '/';
+    const ADD = '+';
+    const SUB = '-';
+    const MULT = '*';
+
+
+    let opChoice = readlineSync.question('What operation would you like to perform? ');
+    if (opChoice !== DIVIDE || ADD || SUB || MULT) {
+        console.log("That is not a valid operation");
+        whichOp();
+    } else {
+        console.log("they've entered a valid operand");
+    }
+}
+
  
-// Handle the secret text (e.g. password).
-var favFood = readlineSync.question('What is your favorite food? ', {
-  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
-});
-console.log('Oh, ' + userName + ' loves ' + favFood + '!');
