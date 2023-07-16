03247e5dd9715b68364e95ac884181a89eafaea8
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-07-15 21:47:44 -0700

    calculate the result and return a result statement
    creates method that performs the operation, then have a seperate function that takes in the array of getNumbers and the operator from the whichOp function. then print the result

--- script.js
+++ script.js
@@ -2,40 +2,26 @@ var readlineSync = require('readline-sync');
  
 // Wait for user's response.
 
+const DIVIDE = '/';
+const ADD = '+';
+const SUB = '-';
+const MULT = '*';
+
 const whichOp = () => {
-    const DIVIDE = '/';
-    const ADD = '+';
-    const SUB = '-';
-    const MULT = '*';
+    // const DIVIDE = '/';
+    // const ADD = '+';
+    // const SUB = '-';
+    // const MULT = '*';
 
 
     //take out next line to adjust program to take entire statement ?
     let opChoice = readlineSync.question("What operation would you like to perform? ");
     if (opChoice === DIVIDE || opChoice === ADD || opChoice === SUB || opChoice === MULT) {
-        console.log("valid operand");
-        getNumbers();
-
+        return opChoice;
     } else {
-        console.log("That is not a valid operation");
         whichOp();
     }
 }
-
-/* const getNumbers = () => {
-     et  numX = readlineSync.question("Please enter a number: ");
-     //if this number is a valid number execute code block
-    const numbersArr = [];
-    let currArrLength = numbersArr.length;
-        if(!isNaN(numX) && currArrLength < 3) {
-            numbersArr.push(numX);
-        } else {
-            console.log("Please enter a valid number.");
-            getNumbers();
-        }
-    console.log(numbersArr);
-}
-  */
-
 
 //take in two numbers and push them to an array
 //call verifNums
@@ -77,4 +63,26 @@ const verifNums = (num) => {
     return isANum; 
 }
 
-whichOp();
+//perform calculation
+const calc = (op, array) => {
+    let result; 
+    if(op == ADD) {
+        result = array[0] + array[1];
+    } else if(op == SUB) {
+        result = array[0] - array[1];
+    } else if(op == MULT) {
+        result = array[0] * array[1];
+    } else if(op == DIVIDE) {
+        result = array[0] / array[1];
+
+    }
+
+    return result.toFixed(2);
+}
+
+const calculatin = () => {
+    let done =  calc(whichOp(), getNumbers()); 
+    return done; 
+}
+
+console.log("The result is: " + calculatin() + "!"); 
