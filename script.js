c2d653f431275b7c7aa52e5f6df5248924916471
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-07-25 22:05:43 -0700

    fixing looping issues with getNums and whichOp
    eliminated verifNums to use isNan instead and added return to else in which ops to return correct operation to be performed

--- script.js
+++ script.js
@@ -1,89 +1,73 @@
 var readlineSync = require('readline-sync');
- 
+
 // Wait for user's response.
 
-const DIVIDE = '/';
-const ADD = '+';
-const SUB = '-';
-const MULT = '*';
+const DIVIDE = "/";
+const ADD = "+";
+const SUB = "-";
+const MULT = "*";
 
 const whichOp = () => {
-    // const DIVIDE = '/';
-    // const ADD = '+';
-    // const SUB = '-';
-    // const MULT = '*';
 
-
-    //take out next line to adjust program to take entire statement ?
-    let opChoice = readlineSync.question("What operation would you like to perform? ");
-    if (opChoice === DIVIDE || opChoice === ADD || opChoice === SUB || opChoice === MULT) {
-        return opChoice;
-    } else {
-        console.log("This is not a valid operation");
-        whichOp();
-    }
+//take out next line to adjust program to take entire statement ?
+  const opChoice = readlineSync.question("What operation would you like to perform? ");
+  if (opChoice === DIVIDE || opChoice === ADD || opChoice === SUB || opChoice === MULT) {
+    return opChoice;
+  } else {
+    console.log("This is not a valid operation");
+    return whichOp();
+  }
 }
 
 //take in two numbers and push them to an array
 //call verifNums
 const getNumbers = () => {
-    const NUMS = [];
-    NUMS.length = 2;
-    const INVALID = "Please enter a valid number. "
+  const NUMS = [];
+  const INVALID = "Please enter a valid number. "
+  
+  while(NUMS.length <= 2){
+    if(NUMS.length === 0) {
+    num = readlineSync.questionInt("Please enter the first number: ");
+    if(isNaN(num)){
+      console.log(INVALID);
+    }else {
+      NUMS.push(num);
+    }
+  }
 
-    for(let i = 0; i < NUMS.length; i++) {
-        while(!NUMS[0]) {
-            num1 = readlineSync.question("Please enter the first number: "); 
-            if(verifNums(num1)){
-                NUMS[0] = num1;
-            } else {
-                console.log(INVALID);
-            }
-        }
-        while(!NUMS[1]) {
-            num2 = readlineSync.question("Please enter the second number: ")
-            if(verifNums(num2)){
-                NUMS[1] = (num2);
-            } else {
-                console.log(INVALID);
-                !NUMS[1];
-            }
-        }
+    if(NUMS.length === 1) {
+      num = readlineSync.questionInt("Please enter the second number: ");
+      if(isNaN(num)){
+        console.log(INVALID);
+      }else {
+        NUMS.push(num);
+      }
     }
-    return NUMS;
-}
-//takes in an array itterates through it and verfies that the elements are 
-//numbers
-const verifNums = (num) => {
-    let isANum = null; 
-    if(!isNaN(num)) {
-        isANum = true;
-    } else {
-        isANum = false;
-    }
-    return isANum; 
+  break;
+  }
+  return NUMS;
 }
 
 //perform calculation
-const calc = (op, array) => {
-    let result; 
-    if(op == ADD) {
-        result = array[0] + array[1];
-    } else if(op == SUB) {
-        result = array[0] - array[1];
-    } else if(op == MULT) {
-        result = array[0] * array[1];
-    } else if(op == DIVIDE) {
-        result = array[0] / array[1];
+const calcu = (op, array) => {
+  let result;
+  if (op === ADD) {
+    result = array[0] + array[1];
+  } else if (op === SUB) {
+    result = array[0] - array[1];
+  } else if (op === MULT) {
+    result = array[0] * array[1];
+  } else if (op === DIVIDE) {
+    result = array[0] / array[1];
 
-    }
+  }
 
-    return result.toFixed(2);
+  return result;
 }
 
 const calculatin = () => {
-    let done =  calc(whichOp(), getNumbers()); 
-    return done; 
+  let done = calcu(whichOp(), getNumbers());
+  return done;
 }
 
-console.log("The result is: " + calculatin() + "!"); 
+console.log("The result is: " + calculatin() + "!");
